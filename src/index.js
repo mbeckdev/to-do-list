41db206921c0d8c6c08bd31c8892ee5e1dc438f1
mbeckdev <mbeckdev42@gmail.com>
2021-05-10 12:34:42 -0600

    Add nav menu movement to offscreen

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 "use strict";
 import css from "./styles.css";
+
+const hamburger = document.getElementById("hamburger");
+hamburger.addEventListener("click", toggleNav);
+
+const nav = document.getElementById("navigation");
+function toggleNav() {
+  console.log("toggling nav");
+  if (nav.classList.contains("nav-is-left")) {
+    nav.classList.remove("nav-is-left");
+  } else {
+    nav.classList.add("nav-is-left");
+  }
+}
