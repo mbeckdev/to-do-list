b5f2925febf66e83922f10e6c4a0bcc7bbc532de
mbeckdev <mbeckdev42@gmail.com>
2021-05-12 10:40:29 -0600

    Add form visibility on click

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 "use strict";
 import css from "./styles.css";
 
+// Move menu in and out on mobile version
 const hamburger = document.getElementById("hamburger");
 hamburger.addEventListener("click", toggleNav);
 
@@ -12,4 +13,23 @@ function toggleNav() {
   } else {
     nav.classList.add("nav-is-left");
   }
+}
+
+// Add-Task Buttons
+document.querySelectorAll(".add-task").forEach((addTaskBtn) => {
+  addTaskBtn.addEventListener("click", showTaskForm);
+});
+
+const form = document.querySelector("form");
+
+function showTaskForm() {
+  console.log("add task btn clicked");
+  form.classList.remove("form-is-hidden");
+}
+
+// submit button clicked
+form.addEventListener("submit", hideTaskForm);
+
+function hideTaskForm() {
+  form.classList.add("form-is-hidden");
 }
